485ce08a4238ba86b7a9258e3bfcace5b876d187
awesomness560 <appsforyou6@gmail.com>
2024-03-24 22:09:28 -0500

    Misc changes

--- Scripts/multiplayer_menu.gd
+++ Scripts/multiplayer_menu.gd
@@ -9,7 +9,7 @@ extends CanvasLayer
 @export var joinButton : Button
 
 @onready var sceneManager : SceneManager = $/root/SceneManager
-#const PORT : int = 5555
+const PORT : int = 7000
 #var enet_peer : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
 func _ready():
@@ -85,10 +85,12 @@ func removePlayer(id):
 	#var discover_result = upnp.discover()
 	#assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
 		#"UPNP Discover Failed! Error %s" % discover_result)
-		#
-	#assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
-		#"UPNP Invalid Gateway!")
-		#
+	#
+	##print(upnp.get_gateway())
+	##
+	##assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		##"UPNP Invalid Gateway!")
+	#
 	#var map_result = upnp.add_port_mapping(PORT)
 	#assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
 		#"UPNP Port Mapping Failed! Error %s" % map_result)
